f54bbbb6390a0a8ab07c6030ce9589ff1a747abc
versha0 <thisisversha@gmail.com>
2019-10-16 10:33:24 +0530

    Lombok

--- src/main/java/com/stackroute/MuzixApplication/domain/Muzix.java
+++ src/main/java/com/stackroute/MuzixApplication/domain/Muzix.java
@@ -1,13 +1,20 @@
 package com.stackroute.MuzixApplication.domain;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
 @Entity
 @Table(name = "versha")
 public class Muzix {
@@ -23,45 +30,45 @@ public class Muzix {
     @Value("Awesome")
     String comment="";
 
-    public Muzix(int id, String name, String comment) {
-        this.id = id;
-        this.name = name;
-        this.comment = comment;
-    }
-
-    public Muzix() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    @Override
-    public String toString() {
-        return "Muzix{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", comment='" + comment + '\'' +
-                '}';
-    }
+//    public Muzix(int id, String name, String comment) {
+//        this.id = id;
+//        this.name = name;
+//        this.comment = comment;
+//    }
+//
+//    public Muzix() {
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getComment() {
+//        return comment;
+//    }
+//
+//    public void setComment(String comment) {
+//        this.comment = comment;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Muzix{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", comment='" + comment + '\'' +
+//                '}';
+//    }
 }
